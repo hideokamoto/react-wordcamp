a0affd6f7377205fa2c36a7089355d27db905db3
hideokamoto <hide.okamoto@digitalcube.jp>
2016-01-01 02:05:19 +0900

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var webserver = require('gulp-webserver');
+
+gulp.task('browserify', function() {
+  browserify('./modules/app.jsx', { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./modules/*.jsx', ['browserify'])
+});
+
+gulp.task('default', ['browserify', 'watch']);
